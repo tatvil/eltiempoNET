0751816d71e2038de58a6daef893459b48ad34e5
Tatiana Villa Ema <tatiana@tecnologia-facil.es>
2026-01-11 17:36:31 +0100

    Conectando a la base de datos

---
+++ Models/Estadistica.cs
@@ -0,0 +1,17 @@
+﻿namespace ElTiempoWeb.Models
+{
+
+    public class Estadistica
+    {
+        public int Id { get; set; }
+        public string Ciudad { get; set; }
+        public int Mes { get; set; }
+        public double TempMax { get; set; }
+        public double TempMedia { get; set; }
+        public double TempMin { get; set; }
+        public double Precipitacion { get; set; }
+        public double Viento { get; set; }
+        public double Humedad { get; set; }
+    }
+
+}
